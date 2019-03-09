bf9e6255a5ba6e09d78e94d472679148d1330c3b
LeeXiao <gary19941103@gmail.com>
2019-03-09 23:40:19 +0800

    Updates

---
+++ precache-manifest.013eb803971e668c4779c753bd4db3d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dbf60055ca4899bccea",
+    "url": "/CookBook/static/js/runtime~main.6e9ba83c.js"
+  },
+  {
+    "revision": "c321adf002be9853777b",
+    "url": "/CookBook/static/js/main.c122cb29.chunk.js"
+  },
+  {
+    "revision": "e7bd3ac03493e001e12c",
+    "url": "/CookBook/static/js/2.70ce752a.chunk.js"
+  },
+  {
+    "revision": "c321adf002be9853777b",
+    "url": "/CookBook/static/css/main.18233223.chunk.css"
+  },
+  {
+    "revision": "861496055e38b16f7be1052a7e9bc870",
+    "url": "/CookBook/index.html"
+  }
+];
